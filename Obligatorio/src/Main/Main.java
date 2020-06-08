c463550b9c9a031891c9a10ba681187a502b5176
renzo14 <renzo.dtm@gmail.com>
2020-06-08 14:55:38 -0300

    'agregaOpencvs'

--- Obligatorio/src/Main/Main.java
+++ Obligatorio/src/Main/Main.java
@@ -1,13 +1,13 @@
+package Main;
+
 import com.opencsv.CSVReader;
 import java.lang.*;
 import java.io.*;
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         // CARGA DE DATOS - INICIO **************************
         try {
-            System.out.println("lpm");
             String archCSV = "Data/books.csv";
             CSVReader csvReader = new CSVReader(new FileReader(archCSV));
             String[] fila = null;
@@ -15,10 +15,29 @@ public class Main {
                 System.out.println(fila[0] + " | " + fila[1]  + " |  " + fila[2]);
             }
             csvReader.close();
+            csvReader = null;
+
+            archCSV = "Data/ratings.csv";
+            CSVReader csvReader2 = new CSVReader(new FileReader(archCSV));
+            while ((fila = csvReader2.readNext()) != null) {
+                System.out.println(fila[0] + " | " + fila[1]  + " |  " + fila[2]);
+            }
+            csvReader2.close();
+            csvReader2 = null;
+
+            archCSV = "Data/to_read.csv";
+            CSVReader csvReader3 = new CSVReader(new FileReader(archCSV));
+            while ((fila = csvReader3.readNext()) != null) {
+                System.out.println(fila[0] + " | " + fila[1]);
+            }
+            csvReader3.close();
+            csvReader3 = null;
+
+
         } catch (Exception e) {
-            System.out.println("error");
+            System.out.println(e);
         }
-        System.out.println("lpm");
+
 
     }
 }
